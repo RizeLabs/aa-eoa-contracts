024888d866869da3ce87a5b0b623aa0e0aa2e2e6
Rishabh Gupta <rishotics@gmail.com>
2023-08-17 12:03:53 +0530

    add test

--- test/Lock.ts
+++ test/Lock.ts
@@ -2,123 +2,143 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { EntryPoint, EntryPoint__factory } from "@account-abstraction/contracts";
+import { BananaAccount } from "../src/types";
 
-describe("Lock", function () {
+
+function parseEther(eth: string) {
+  return ethers.utils.parseEther(eth);
+}
+
+describe("Smart Account tests", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+  let entryPoint: EntryPoint;
+  let baseImpl: BananaAccount;
+  let walletFactory: any;
+  let token: any;
+  let owner: string;
+  let bob: any;
+  let userSCW: any;
+  let account: any;
+  let accounts: any;
+  let tx: any;
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  before(async () => {
+    const [account, bob] = await ethers.getSigners();
+    console.log("account: ", account);
+    console.log("bob: ", bob);
+    console.log(await ethers.getSigners());
+    let balance = await ethers.provider.getBalance(
+      account.getAddress()
+    );
+    // expect(balance).to.equal(0);
+    console.log("balance1: ", balance.toString())
+    balance = await ethers.provider.getBalance(
+      bob.getAddress()
+    );
+    // expect(balance).to.equal(0);
+    console.log("balance bob: ", balance.toString())
+    const epf = await (await ethers.getContractFactory("EntryPoint")).deploy();
+    entryPoint = await EntryPoint__factory.connect(epf.address, accounts[0]);
+    console.log("entry point deployed at: ", entryPoint.address);
 
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    owner = await accounts[0].address;
+    console.log("owner address: ", owner); 
+    bob = await accounts[1];
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const BaseImplementation = await ethers.getContractFactory("BananaAccount");
+    baseImpl = await BaseImplementation.deploy();
+    await baseImpl.deployed();
+    console.log("base wallet impl deployed at: ", baseImpl.address);
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
-  }
+    const WalletFactory = await ethers.getContractFactory(
+      "BananaAccountProxyFactory"
+    );
+    walletFactory = await WalletFactory.deploy();
+    await walletFactory.deployed();
+    console.log("wallet factory deployed at: ", walletFactory.address);
 
-  describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+    const MockToken = await ethers.getContractFactory("MyToken");
+    token = await MockToken.deploy();
+    await token.deployed();
+    console.log("Test token deployed at: ", token.address);
 
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
-    it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
-    });
+    // console.log("mint tokens to owner address..");
+    // await token.mint(owner, ethers.utils.parseEther("1000000"));
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("transfer: take native tokens out of Smart Account", function () {
+    it("success if enough tokens and owner call", async () => {
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
+      console.log("create smart account for owner..");
+
+      
+      const initializer = baseImpl.interface.encodeFunctionData('setupWithEntrypoint',
+                [
+                owner, // owners 
+                1,                                              // thresold will remain fix 
+                "0x0000000000000000000000000000000000000000",   // to address 
+                "0x",                                           // modules setup calldata
+                "0xac1c08a5a59cEA20518f7201bB0dda29d9454eb0",          // fallback handler
+                "0x0000000000000000000000000000000000000000",   // payment token
+                0,                                              // payment 
+                "0x0000000000000000000000000000000000000000",   // payment receiver
+                entryPoint.address                // entrypoint         // q values 
+                ]);
+      console.log("initializer: ", initializer);
+      const scwAddress = await walletFactory.getAddress(baseImpl.address, "0", initializer);
+      console.log("smart account created at: ", scwAddress);
+      tx = await walletFactory.createProxyWithNonce(baseImpl.address, initializer, "0");
+      const receipt = await tx.wait();
+      const proxyCreationEvent = receipt.events.find(
+        (event: any) => event.event === "ProxyCreation"
         );
+      const [proxy, singleton] = proxyCreationEvent.args;
+      console.log("proxy : ", proxy)
+      expect(proxy).to.equal(scwAddress);
+
+      let balance = await ethers.provider.getBalance(
+        scwAddress
+      );
+      expect(balance).to.equal(0);
+      console.log("balance: ", balance.toString())
+
+      balance = await ethers.provider.getBalance(
+        accounts[0].address
+      );
+      // expect(balance).to.equal(0);
+      console.log("balance1: ", balance.toString())
+
+      tx = await accounts[0].sendTransaction({
+        to: scwAddress,
+        value: parseEther("1"),
       });
+      await tx.wait();
+      balance = await ethers.provider.getBalance(scwAddress);
+      expect(balance).to.equal(parseEther("1"));
+      console.log("balance: ", balance.toString())
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+      // transfer 0.5 ETH from smart account to bob by owner signature
+      const bobBalanceBefore = await ethers.provider.getBalance(bob);
+      userSCW = await ethers.getContractAt(
+        "SmartAccount",
+        scwAddress
+      );
+      tx = await userSCW.connect(accounts[0]).transfer(bob, parseEther("0.5"));
+      await tx.wait();
 
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
+      expect(await ethers.provider.getBalance(bob)).to.equal(
+        bobBalanceBefore.add(parseEther("0.5"))
+      );
+      expect(
+        await ethers.provider.getBalance(scwAddress)
+      ).to.equal(parseEther("0.5"));
 
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
 
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
-    });
-
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
-    });
   });
+  
 });
